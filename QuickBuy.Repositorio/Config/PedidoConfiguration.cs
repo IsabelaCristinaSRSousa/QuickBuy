ce64787199a36ca73a6a3ab3314072aa886d4050
Isa Cristina <sisabelacris@hotmail.com>
2020-04-26 19:43:26 -0300

    Relacionamentos

---
+++ QuickBuy.Repositorio/Config/PedidoConfiguration.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using QuickBuy.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Repositorio.Config
+{
+    public class PedidoConfiguration : IEntityTypeConfiguration<Pedido>
+    {
+        public void Configure(EntityTypeBuilder<Pedido> builder)
+        {
+            builder.HasKey(pd => pd.Id);
+
+            builder
+                .Property(pd => pd.DataPedido)
+                .IsRequired();           
+
+            builder
+                .Property(pd => pd.DataPrevisaoEntrega)
+                .IsRequired();
+            builder
+                .Property(pd => pd.Cep)
+                .IsRequired()
+                .HasMaxLength(10);
+
+            builder
+                .Property(pd => pd.Estado)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder
+                .Property(pd => pd.EnderecoCompleto)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            builder
+                .Property(pd => pd.NumeroEndereco)
+                .IsRequired();
+
+            builder.HasOne(p => p.Usuario);
+            
+
+
+                
+        }
+    }
+}
--- QuickBuy.Web/Startup.cs
+++ QuickBuy.Web/Startup.cs
@@ -26,7 +26,7 @@ namespace QuickBuy.Web
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             var connetionString = Configuration.GetConnectionString("QuickBuyDB");
-            services.AddDbContext<QuickBuyContexto>(option => option.UseMySql(connetionString, m => m.MigrationsAssembly("QuickBuy.Repositorio")));
+            services.AddDbContext<QuickBuyContexto>(option => option.UseLazyLoadingProxies().UseMySql(connetionString, m => m.MigrationsAssembly("QuickBuy.Repositorio")));
 
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
